b345d5df0ab4dae398f0a42e9e5a84e0cbbbd1cd
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-01-10 18:22:34 +0900

    전문회원 기준 변경

--- data/schema/user_db/create_user_level_tbl.sql
+++ data/schema/user_db/create_user_level_tbl.sql
@@ -2,5 +2,5 @@ CREATE TABLE user_db.user_level_tbl (
     user_level_idx INT AUTO_INCREMENT PRIMARY KEY,
     user_level_name ENUM('General', 'ProPen', 'ProApr', 'ProDecl') NOT NULL,
     user_level_desc VARCHAR(100),
-    user_license ENUM('일반', '약사면허', '의사면허') DEFAULT '일반'
+    user_license ENUM('일반', '약사면허') DEFAULT '일반'
 );
